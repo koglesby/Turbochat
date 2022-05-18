e4b63a04305f51db8850eae5e1fd751c09e0a660
koglesby <kevinjoglesby@gmail.com>
2022-05-18 13:30:04 -0700

    stimulus controller scroll down

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  connect() {
+    console.log('connected');
+    const messages = document.getElementById('messages');
+    messages.addEventListener('DOMNodeInserted', this.resetScroll);
+    this.resetScroll(messages);
+  }
+
+  disconnect() {
+    console.log('disconnected');
+  }
+
+  // custom function (not from stimulus)
+  resetScroll() {
+    messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+  }
+}
