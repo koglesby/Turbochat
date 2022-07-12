fbb5e29301e451e94fb0393605811ce3aea5e4a9
koglesby <kevinjoglesby@gmail.com>
2022-07-12 14:57:14 -0700

    change production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.active_storage.variant_processor = :mini_magick
+  # config.active_storage.variant_processor = :mini_magick
 
   host = ENV['DEFAULT_URL_HOST'] || "#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
   protocol = config.force_ssl ? 'https' : 'http'
