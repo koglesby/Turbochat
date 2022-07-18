3c214b4ab2b06014b4b6ca148122057eeb372f7f
koglesby <kevinjoglesby@gmail.com>
2022-07-18 13:56:24 -0700

    add seeds for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+kev = User.create(
+  email: 'kev@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: 'admin'
+)
+User.create(
+  email: 'jon@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+User.create(
+  email: 'jan@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+kev.joined_rooms << Room.create(name: 'General', is_private: false)
+kev.joined_rooms << Room.create(name: 'Testing', is_private: false)
